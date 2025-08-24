36a77985b7b6a04a8854a26ec98937561d10957b
Laurids <49726628+Jibs23@users.noreply.github.com>
2025-08-24 17:46:42 +0200

    Add sprites
    - Add bucket sprite
    - Add bumper sprite and treasure piles
    - Add bucket particles

--- Entities/Bucket/bucket.gd
+++ Entities/Bucket/bucket.gd
@@ -4,6 +4,7 @@ signal bucket_scored
 
 var bucket_track: Path2D
 var bucket_path_follow: PathFollow2D
+var splash: GPUParticles2D
 
 func _init() -> void:
 	Logic.bucket = self
@@ -15,6 +16,7 @@ func _ready() -> void:
 		bucket_path_follow = bucket_track.get_node("PathFollow2D")
 		bucket_path_follow.bucket = self
 	bucket_path_follow.bucket = self
+	splash = $SplashGPU
 
 func _on_score_box_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Ball"):
@@ -29,6 +31,7 @@ func bucket_score_valid(ball) -> void:
 	ball.ballMissed = false  # Mark the ball as not missed
 	ball.endBall()  # Remove the ball from the scene
 	Logic.audio.playSoundEffect("SFXBucketScore")
+	splash.emitting = true
 
 func on_bonus_mode_activated() -> void:
 	queue_free()
